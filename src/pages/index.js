678c38a6a6b34c3a5ba5e468f117bab26a970b93
Haris Rozajac <haris.rozajac@fluentworlds.com>
2020-03-19 10:08:30 -0600

    Deleted all unnecessary stuff to start from scratch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
+import Navbar from ".components/Navbar.js"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Municipal transcription services</h1>
-    <p>We offer exceptional municipal transcription services</p>
-    <p>We are building something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/contact/">Contact</Link>
-  </Layout>
-)
-
-export default IndexPage
+Navbar
