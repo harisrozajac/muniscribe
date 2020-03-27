df923be15ece1998e4e6ddca79c547e865c58c8d
Haris Rozajac <haris.rozajac@fluentworlds.com>
2020-03-27 12:54:20 -0600

    Test layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,11 @@
 import React from "react"
+import Navbar from "./Navbar"
 
 export default ({ children }) => (
-  <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
-    {children}
-  </div>
+  <>
+    <Navbar />
+    <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+      {children}
+    </div>
+  </>
 )
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,17 +1,8 @@
 import React, { Component } from "react"
-import Navbar from "../components/Navbar"
 import ContactForm from "../components/ContactForm"
 
 export default class ContactPage extends Component {
   render() {
-    return (
-      <>
-        <Navbar />
-        <div>
-          <p> Contact </p>{" "}
-        </div>
-        <ContactForm />
-      </>
-    )
+    return <ContactForm />
   }
 }
