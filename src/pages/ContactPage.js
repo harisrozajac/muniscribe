1943748cb5d5722a8421da932fc3610e66137048
Haris Rozajac <haris.rozajac@fluentworlds.com>
2020-03-27 13:32:29 -0600

    Add css

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import ContactForm from "../components/ContactForm"
+import "../pages/App.css"
 
 export default class ContactPage extends Component {
   render() {
