3b1dd491cf83000d81bf0948d81ec051770aa9fc
Haris Rozajac <haris.rozajac@fluentworlds.com>
2020-07-02 18:02:37 -0600

    Fixing footer with negative margins

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,18 @@ import styled from "styled-components"
 
 const FooterContainer = styled.div`
   margin-top: 1em;
-  position: static;
+  position: relative;
   left: 0;
-  bottom: 0;
+  bottom: -8em;
   width: 100%;
   background-color: rgb(255, 255, 255);
   color: black;
   text-align: center;
   height: 2em;
+
+  p {
+    font-size: 14px;
+  }
 `
 
 const Footer = () => {
--- src/pages/About.js
+++ src/pages/About.js
@@ -50,42 +50,42 @@ const ImageContainer = styled.img`
   }
 `
 
-export default class About extends Component {
-  render() {
-    return (
-      <>
-        <Layout>
-          <Container>
+const About = () => {
+  return (
+    <>
+      <Layout>
+        <Container>
+          <Text>
+            At MuniScribe, we work with you to bring your minutes to date, stay
+            current, edit and format meeting minutes. Our intent is to provide
+            quality municipal transcription, meanwhile you are able to stay
+            efficient while we do your busy work.
+          </Text>
+          <RowContainer>
+            <ImageContainer src={morgan} />
             <Text>
-              At MuniScribe, we work with you to bring your minutes to date,
-              stay current, edit and format meeting minutes. Our intent is to
-              provide quality municipal transcription, meanwhile you are able to
-              stay efficient while we do your busy work.
+              Owner, Morgan Brim obtained a master's degree in city and
+              metropolitan planning from the University of Utah and is a member
+              of the American Institute of Certified Planners. He has worked in
+              municipal government for 10 years providing him great insight into
+              the requirements for effective meeting minutes.
             </Text>
-            <RowContainer>
-              <ImageContainer src={morgan} />
-              <Text>
-                Owner, Morgan Brim obtained a master's degree in city and
-                metropolitan planning from the University of Utah and is a
-                member of the American Institute of Certified Planners. He has
-                worked in municipal government for 10 years providing him great
-                insight into the requirements for effective meeting minutes.
-              </Text>
-            </RowContainer>
-            <RowContainer>
-              <ImageContainer src={brooke} />
-              <Text>
-                Brooke Brim obtained a Bachelor of Arts from the University of
-                Utah in English language and literature. She has worked as an
-                office assistant, media manager and transcriber. Currently, her
-                primary focus is MuniScribe and meeting each clients needs
-                thoroughly. She understands the importance of a quality and
-                complete document.
-              </Text>
-            </RowContainer>
-          </Container>
-        </Layout>
-      </>
-    )
-  }
+          </RowContainer>
+          <RowContainer>
+            <ImageContainer src={brooke} />
+            <Text>
+              Brooke Brim obtained a Bachelor of Arts from the University of
+              Utah in English language and literature. She has worked as an
+              office assistant, media manager and transcriber. Currently, her
+              primary focus is MuniScribe and meeting each clients needs
+              thoroughly. She understands the importance of a quality and
+              complete document.
+            </Text>
+          </RowContainer>
+        </Container>
+      </Layout>
+    </>
+  )
 }
+
+export default About
